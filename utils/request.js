9705d35909315a800aaa925dc1b0c94c1e475c0d
南京-何元梅 <heym57@chinaunicom.cn>
2023-07-05 17:26:06 +0800

    fix: error in if... else if

--- utils/request.js
+++ utils/request.js
@@ -6,19 +6,17 @@ export const request = ((options) => { //options是json对象
 			url: BASE_URL + options.url,
 			method: options.method || 'GET',
 			header: {
-				token: uni.getStorageSync('user') ? uni.getStorageSync('user').token : ''
+				token: uni.getStorageSync('user') ? uni.getStorageSync('user').username : ''
 			},
 			data: options.data || {},
 			success: (res) => {
 				const data = res.data;
-				if (data.code === '401') { //401无权限
+				if (data.code === 401) { //401无权限
 					uni.navigateTo({
 						url: '/pages/login/login'
 					})
 					return
-				} else if(data.code=2001){
-					
-				}else if (data.code !== '200') {
+				}else if (data.code !== 200) {
 					uni.showToast({
 						icon: 'error',
 						title: '操作错误'
