0b0abb25c3980add165eaf31d78fdc0a9d17991a
南京-何元梅 <heym57@chinaunicom.cn>
2023-07-06 15:52:43 +0800

    feat: 完成首页页面设计

--- pages/components/orderDishCard.vue
+++ pages/components/orderDishCard.vue
@@ -1,13 +1,20 @@
 <template>
 	<view class="container">
-		 <view class="order-dish">
-		    
-			  <text>这是一个基础卡片示例，此示例展示了一个标题加标题额外信息的标准卡片。</text>
-		
-		  </view>
-		  <view class="score-card">
-			  <text>这是一个基础卡片示例，此示例展示了一个标题加标题额外信息的标准卡片。</text>
-		  </view>
+		<view class="order-dish">
+			<view class="content">
+				<text>点餐</text>
+				<text class="clamp-text">在线点餐免排队</text>
+				<image class="order_image" src="/static/home/order.png"  /> 
+			</view>
+		</view>
+		<view class="score-card">
+			<view class="content">
+				<text>积分兑换</text>
+				<text class="clamp-text">1积分=1￥<br /></text>
+				<text class="clamp-text">免费兑换手抓饼</text>
+				<image class="integral_image" src="/static/home/兑换.png"  /> 
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -15,8 +22,8 @@
 	export default {
 		name: 'OrderDishCard',
 		data() {
-			return { 
-				image: "/static/announcement/announcement.png"
+			return {
+				
 			};
 		},
 		methods: {}
@@ -24,27 +31,62 @@
 </script>
 
 <style>
-	.container{
+	.container {
 		display: flex;
-		justify-content: center;                                            
+		justify-content: center;
 		align-items: center;
 	}
+
 	.score-card {
 		width: 300rpx;
 		height: 250rpx;
 		margin-left: 80rpx;
-	    background-color: #ffebd9;
-		color: #e5b267;
-	    border-radius: 10px;
-	    box-shadow: 0 2px 6px 0 rgba(229, 178, 103, 0.2);
-	  }
+		/* background-color: #ffebd9; */
+		color: #f9b653;
+		border-radius: 10px;
+		border-color: #e5b267;
+		font-size: 30rpx;
+		box-shadow: 0 2px 6px 0 rgba(229, 178, 103, 0.2);
+	}
+
 	.order-dish {
 		width: 300rpx;
 		height: 250rpx;
 		margin-left: 0rpx;
-		color: #e5b267;
-		background-color: #ffefd3;
+		color: #f9b653;
+		/* background-color: #ffefd3; */
 		border-radius: 10px;
-		box-shadow: 0 2rpx 6rpx 0 rgba(0, 0, 0, 0.16);
+		border-color: #e5b267;
+		font-size: 30rpx;
+		box-shadow: 0 6rpx 24rpx 0 rgba(229, 178, 103, 0.2);
+	}
+
+	.content {
+		margin-left: 30rpx;
+		margin-top: 20rpx;
+	}
+
+	.clamp-text {
+		display: -webkit-box;
+		-webkit-line-clamp: 2;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		font-size: 25rpx;
+		margin-top: 10rpx;
+	}
+	.order_image {
+		width: 80rpx;
+		height: 70rpx;
+		padding-left: 150rpx;
+		padding-top: 60rpx;
+	}
+	.integral_image {
+		display: flex;
+		width: 70rpx;
+		height: 70rpx;
+		margin-left: 170rpx;
+		/* margin-top: 50rpx; */
+		padding-top: 30rpx;
+		
 	}
 </style>
--- pages/homepage/index.vue
+++ pages/homepage/index.vue
@@ -25,35 +25,35 @@
 		},
 		data() {
 			return {
-				tabList: [{
-						index: 0,
-						pagePath: '/pages/homepage/index',
-						iconPath: '/static/tabbar/home.png',
-						selectedIconPath: '/static/tabbar/home(1).png',
-						text: '首页'
-					},
-					{
-						index: 1,
-						pagePath: '/pages/food/food',
-						iconPath: '/static/tabbar/food.png',
-						selectedIconPath: '/static/tabbar/food(1).png',
-						text: '点餐'
-					},
-					{
-						index: 2,
-						pagePath: '/pages/order/order',
-						iconPath: '/static/tabbar/order.png',
-						selectedIconPath: '/static/tabbar/order(1).png',
-						text: '订单'
-					},
-					{
-						index: 3,
-						pagePath: '/pages/my/my',
-						iconPath: '/static/tabbar/my.png',
-						selectedIconPath: '/static/tabbar/my(1).png',
-						text: '我的'
-					}
-				]
+				// tabList: [{
+				// 		index: 0,
+				// 		pagePath: '/pages/homepage/index',
+				// 		iconPath: '/static/tabbar/home.png',
+				// 		selectedIconPath: '/static/tabbar/home(1).png',
+				// 		text: '首页'
+				// 	},
+				// 	{
+				// 		index: 1,
+				// 		pagePath: '/pages/food/food',
+				// 		iconPath: '/static/tabbar/food.png',
+				// 		selectedIconPath: '/static/tabbar/food(1).png',
+				// 		text: '点餐'
+				// 	},
+				// 	{
+				// 		index: 2,
+				// 		pagePath: '/pages/order/order',
+				// 		iconPath: '/static/tabbar/order.png',
+				// 		selectedIconPath: '/static/tabbar/order(1).png',
+				// 		text: '订单'
+				// 	},
+				// 	{
+				// 		index: 3,
+				// 		pagePath: '/pages/my/my',
+				// 		iconPath: '/static/tabbar/my.png',
+				// 		selectedIconPath: '/static/tabbar/my(1).png',
+				// 		text: '我的'
+				// 	}
+				// ]
 			}
 		},
 		onLoad() {},
