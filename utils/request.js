d15d6b04a0b3cc646ecaefab7e4b2fde04295e7e
南京-何元梅 <heym57@chinaunicom.cn>
2023-08-11 10:45:34 +0800

    fix: baseUrl

--- utils/request.js
+++ utils/request.js
@@ -1,5 +1,5 @@
-// const BASE_URL = 'https://hym.mhyt.fun'
-const BASE_URL = 'http://localhost:9090'
+ // const BASE_URL = 'https://hym.mhyt.fun'
+const BASE_URL = 'http://hym.mhyt.fun:9090'
 
 export const request = ((options) => { //options是json对象
 	return new Promise((resolve, reject) => {
