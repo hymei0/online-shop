4a1fca717bf8c4df586170e550300b961be9631c
南京-何元梅 <heym57@chinaunicom.cn>
2023-08-31 11:16:57 +0800

    fix: update addshopCardata load to food.vue

--- pages/food/food.vue
+++ pages/food/food.vue
@@ -6,6 +6,9 @@
 
 <script>
 	import FoodType from './components/foodType.vue'
+	import store from '@/utils/store.js'
+	store.dispatch('fetchshopCarData') // 发起异步请求获取后端数据
+	
 	export default {
 		name: 'Food',
 		components: {
